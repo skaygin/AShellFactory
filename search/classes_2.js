d2649aa80fccd19a06498ecc0de0cb7cc75ca49b
skaygin <skaygin@users.noreply.github.com>
2022-06-24 09:59:21 +0000

    deploy: 5a9590e37efa07474864b130f232dc80512a8ad4

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['eepromclass_0',['EEPROMClass',['../class_e_e_p_r_o_m_class.html',1,'']]],
-  ['endpointnode_1',['EndpointNode',['../class_endpoint_node.html',1,'']]]
+  ['eepromclass_0',['EEPROMClass',['../class_e_e_p_r_o_m_class.html',1,'']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['shellcommand_0',['ShellCommand',['../struct_shell_command.html',1,'']]],
+  ['shellcommandstruct_0',['ShellCommandStruct',['../struct_shell_command_struct.html',1,'']]],
   ['shellcontroller_1',['ShellController',['../class_shell_controller.html',1,'']]],
   ['shellframing_2',['ShellFraming',['../class_shell_framing.html',1,'']]]
 ];
